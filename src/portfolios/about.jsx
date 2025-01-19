9f46e0f6002ed85518046c0473d536021560e9cd
Abhishek Mishra <166637489+mrabhishekmishra@users.noreply.github.com>
2025-01-19 19:58:08 +0530

    photo update

--- src/portfolios/about.jsx
+++ src/portfolios/about.jsx
@@ -31,7 +31,7 @@ const about = () => {
    <div className="container p-5 mt-[50px] m-auto block md:flex md:justify-between   text-[white] ">
     <div className='w-[60%] text-center lg:text-start m-auto '>
     <div className="img w-[100%]  flex justify-center m-auto  block lg:hidden items-center  ">
-      <div className="profile-img border border-[cyan]  w-[500px] h-[380px] rounded-[50%] overflow-hidden ">
+      <div className="profile-img border border-[cyan]  w-[100%]  rounded-[50%] overflow-hidden ">
     <img src={Img} alt="" className=' w-[100%]'/>
       </div>
     </div>
